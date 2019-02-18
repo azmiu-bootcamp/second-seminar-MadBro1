5775570639b43ab783e8c78247ecd3a9ff9ab03b
Aslan <47611260+MadBro1@users.noreply.github.com>
2019-02-18 13:42:04 -0800

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,1 +1,329 @@
+n=input('1 ve 100 arasinda bir reqem daxil edin: ')
+if int(n)<=10:
+    if n=='1':
+        print("Bir")
+    elif n=='2':
+        print("Iki")
+    elif n=='3':
+         print("Uc")
+    elif n=='4':
+        print("dord")
+         
+    elif n=='5':
+        print("bes")
+          
+    elif n=='6':
+        print("alti")
+         
+    elif n=='7':   
+        print("yeddi")
+         
+    elif n=='8':    
+        print("sekkiz")
+         
+    elif n=='9':     
+        print("doqquz")
+         
+    elif n=='10':     
+        print("on")
+if int(n)>10 and int(n)<=20:
+     if n[1]=='1':
+        print("on bir")
+     elif n[1]=='2':
+        print("on iki")
+     elif n[1]=='3':
+         print("on uc")
+     elif n[1]=='4':
+        print("on dord")
+         
+     elif n[1]=='5':
+        print(" on bes")
+          
+     elif n[1]=='6':
+        print("on alti")
+         
+     elif n[1]=='7':   
+        print("on yeddi")
+         
+     elif n[1]=='8':    
+        print("on sekkiz")
+         
+     elif n[1]=='9':     
+        print("on doqquz")
+     elif n=='20':
+        print("Iyirmi")
+if int(n)>20 and int(n)<=30:
+     if n[1]=='1':
+        print("iyirmi bir")
+     elif n[1]=='2':
+        print("iyirmi iki")
+     elif n[1]=='3':
+         print("iyirmi uc")
+     elif n[1]=='4':
+        print("iyirmi dord")
+         
+     elif n[1]=='5':
+        print(" iyirmi bes")
+          
+     elif n[1]=='6':
+        print("iyirmi alti")
+         
+     elif n[1]=='7':   
+        print("iyirmi yeddi")
+         
+     elif n[1]=='8':    
+        print("iyirmi sekkiz")
+        
+     elif n[1]=='9':     
+        print("iyirmi doqquz")
+     elif n=='30':
+        print("otuz")
+if int(n)>30 and int(n)<=40:
+    if n[1]=='1':
+        print("otuz bir")
+    elif n[1]=='2':
+        print("otuz iki")
+    elif n[1]=='3':
+         print("otuz uc")
+    elif n[1]=='4':
+        print("otuz dord")
+         
+    elif n[1]=='5':
+        print(" otuz bes")
+          
+    elif n[1]=='6':
+        print("otuz alti")
+         
+    elif n[1]=='7':   
+        print("otuz yeddi")
+         
+    elif n[1]=='8':    
+        print("otuz sekkiz")
+        
+    elif n[1]=='9':     
+        print("otuz doqquz")
+    elif n=='40':
+        print("qirx")
+if int(n)>40 and int(n)<=50: 
+    if n[1]=='1':
+        print("qirx bir")
+    elif n[1]=='2':
+        print("qirx iki")
+    elif n[1]=='3':
+         print("qirx uc")
+    elif n[1]=='4':
+        print("qirx dord")
+         
+    elif n[1]=='5':
+        print(" qirx bes")
+          
+    elif n[1]=='6':
+        print("qirx alti")
+         
+    elif n[1]=='7':   
+        print("qirx yeddi")
+         
+    elif n[1]=='8':    
+        print("qirx sekkiz")
+        
+    elif n[1]=='9':     
+        print("qirx doqquz")
+    elif n=='50':        
+         print('elli')
+if int(n)>50 and int(n)<=60:
+    if n[1]=='1':
+        print("elli bir")
+    elif n[1]=='2':
+        print("elli iki")
+    elif n[1]=='3':
+         print("elli uc")
+    elif n[1]=='4':
+        print("elli dord")
+         
+    elif n[1]=='5':
+        print(" elli bes")
+          
+    elif n[1]=='6':
+        print("elli alti")
+         
+    elif n[1]=='7':   
+        print("elli yeddi")
+         
+    elif n[1]=='8':    
+        print("elli sekkiz")
+        
+    elif n[1]=='9':     
+        print("elli doqquz")
+    elif n=='60':        
+         print('altmis')
+if int(n)>60 and int(n)<=70:
+    if n[1]=='1':
+        print("altmis bir")
+    elif n[1]=='2':
+        print("altmis iki")
+    elif n[1]=='3':
+         print("altmis uc")
+    elif n[1]=='4':
+        print("altmis dord")
+         
+    elif n[1]=='5':
+        print(" altmis bes")
+          
+    elif n[1]=='6':
+        print("altmis alti")
+         
+    elif n[1]=='7':   
+        print("altmis yeddi")
+         
+    elif n[1]=='8':    
+        print("altmis sekkiz")
+        
+    elif n[1]=='9':     
+        print("altmis doqquz")
+    elif n=='70':        
+         print('yetmis')
+if int(n)>70 and int(n)<=80:
+    if n[1]=='1':
+        print("yetmis bir")
+    elif n[1]=='2':
+        print("yetmis iki")
+    elif n[1]=='3':
+         print("yetmis uc")
+    elif n[1]=='4':
+        print("yetmis dord")
+         
+    elif n[1]=='5':
+        print(" yetmis bes")
+          
+    elif n[1]=='6':
+        print("yetmis alti")
+         
+    elif n[1]=='7':   
+        print("yetmis yeddi")
+         
+    elif n[1]=='8':    
+        print("yetmis sekkiz")
+        
+    elif n[1]=='9':     
+        print("yetmis doqquz")
+    elif n=='80':        
+         print('seksen')
+if int(n)>80 and int(n)<=90: 
+    if n[1]=='1':
+        print("seksen bir")
+    elif n[1]=='2':
+        print("seksen iki")
+    elif n[1]=='3':
+         print("seksen uc")
+    elif n[1]=='4':
+        print("seksen dord")
+         
+    elif n[1]=='5':
+        print(" seksen bes")
+          
+    elif n[1]=='6':
+        print("seksen alti")
+         
+    elif n[1]=='7':   
+        print("seksen yeddi")
+         
+    elif n[1]=='8':    
+        print("seksen sekkiz")
+        
+    elif n[1]=='9':     
+        print("seksen doqquz")
+    elif n=='90':        
+         print('doxsan')
+if int(n)>90 and int(n)<=100: 
+    if n[1]=='1':
+        print("doxsan bir")
+    elif n[1]=='2':
+        print("doxsan iki")
+    elif n[1]=='3':
+         print("doxsan uc")
+    elif n[1]=='4':
+        print("doxsan dord")
+         
+    elif n[1]=='5':
+        print(" doxsan bes")
+          
+    elif n[1]=='6':
+        print("doxsan alti")
+         
+    elif n[1]=='7':   
+        print("doxsan yeddi")
+         
+    elif n[1]=='8':    
+        print("doxsan sekkiz")
+        
+    elif n[1]=='9':     
+        print("doxsan doqquz")
+    elif n=='100':        
+         print('Yuz')
 
+         
+      
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+                 
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
